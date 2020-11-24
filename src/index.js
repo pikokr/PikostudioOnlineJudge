c1b4d817d4ffc7ad7a14e3b768d7ea122b338192
pikokr <parangee9706@gmail.com>
2020-11-24 10:42:36 +0900

    view

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ const app = express()
 
 app.set('view engine', 'pug')
 
+app.set('views', './views')
+
+app.use('/static', express.static('./static'))
+
+app.use('/', require('./routes'))
+
 app.listen(3000, () => console.log('Listening'))

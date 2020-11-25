a2c272e3ad2a9615b36aa83cdd0f53216136b1a7
pikokr <parangee9706@gmail.com>
2020-11-25 22:07:48 +0900

    admin view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const loginRequired = require('../middlewares/loginRequired')
+const adminRequired = require('../middlewares/adminOnly')
 
 router.get('/', (req, res) => {
     res.render('main/index')
 })
 
 router.use('/auth', require('./auth'))
-router.use('/admin', loginRequired, require('./admin'))
+router.use('/admin', loginRequired, adminRequired, require('./admin'))
 
 module.exports = router

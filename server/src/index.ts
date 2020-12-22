da491fc967503da94734df8caa07a0611abe9536
pikokr <parangee9706@gmail.com>
2020-12-22 12:13:20 +0900

    feat: basic apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world!')
+import { ApolloServer } from "apollo-server"
+import config from '../config.json'
+import schema from './schema'
+
+const server = new ApolloServer({
+    schema
+})
+
+server.listen(config.server.port, () => console.log('Listening'))

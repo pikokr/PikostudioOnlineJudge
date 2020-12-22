87bb98091e8bc349de1e406132b84f2cc7305e8f
pikokr <parangee9706@gmail.com>
2020-12-22 16:22:58 +0900

    feat: go home on click title

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
       <Navbar bg="primary" variant="dark">
-        <Navbar.Brand>PIKOSTUDIO JUDGE</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          PIKOSTUDIO JUDGE
+        </Navbar.Brand>
       </Navbar>
     </>
   )

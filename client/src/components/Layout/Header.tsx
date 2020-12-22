257504f1f202945b72c8f8a7b1e2eb92a1857f7f
pikokr <parangee9706@gmail.com>
2020-12-22 16:26:53 +0900

    feat: add some links to nav

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
-      <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark" expand="lg">
         <Navbar.Brand as={Link} to="/">
           PIKOSTUDIO JUDGE
         </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <Nav.Item>
+              <Nav.Link>문제</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link>로그인</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   )

404e9bec3e2f6f5b77838113d36f134521a9a3fa
pikokr <parangee9706@gmail.com>
2020-12-22 13:38:25 +0900

    feat: return token when succeed

--- server/src/schema/resolvers/Mutation/login.ts
+++ server/src/schema/resolvers/Mutation/login.ts
@@ -1,6 +1,8 @@
 import { IFieldResolver } from 'apollo-server'
 import User from '../../../models/User'
 import { generate } from '../../../util/password'
+import jwt from 'jsonwebtoken'
+import config from '../../../../config.json'
 
 export default (async (source, { id, password }) => {
   const user = await User.findOne({ id })
@@ -8,4 +10,5 @@ export default (async (source, { id, password }) => {
   const pw = generate(password, user.salt)
   const pw2 = generate(user.password, user.salt)
   if (pw !== pw2) return null
+  return jwt.sign(user.id, config.jwtSecret)
 }) as IFieldResolver<any, any>

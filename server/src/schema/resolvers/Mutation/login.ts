53dba326bcc97c03f36ee5163b0859dc6950a79f
pikokr <parangee9706@gmail.com>
2020-12-23 16:23:05 +0900

    feat: set createdAt in token

--- server/src/schema/resolvers/Mutation/login.ts
+++ server/src/schema/resolvers/Mutation/login.ts
@@ -6,11 +6,8 @@ import config from '../../../../config.json'
 
 export default (async (source, { id, password }) => {
   const user = await User.findOne({ id })
-  console.log(1)
   if (!user) return null
   const pw = generate(password, user.salt)
-  console.log(2)
   if (pw !== user.password) return null
-  console.log(3)
-  return jwt.sign(user.id, config.jwtSecret)
+  return jwt.sign({id: user.id, createdAt: Date.now()}, config.jwtSecret)
 }) as IFieldResolver<any, any>

f62048dbe96b747d54dd43fc9fd9bf0bc5b70fc1
pikokr <parangee9706@gmail.com>
2020-12-23 20:15:37 +0900

    feat: problem type

---
+++ server/src/schema/typeDefs/Problem.ts
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag'
+
+export default gql`
+  type Problem {
+      id: String!
+      examples: [IO!]!
+  }
+  type IO {
+      input: String!
+      output: String!
+  }
+`
--- server/src/schema/typeDefs/index.ts
+++ server/src/schema/typeDefs/index.ts
@@ -2,5 +2,6 @@ import Query from './Query'
 import Mutation from './Mutation'
 import Me from './Me'
 import User from './User'
+import Problem from './Problem'
 
-export default [Query, Mutation, Me, User]
+export default [Query, Mutation, Me, User, Problem]

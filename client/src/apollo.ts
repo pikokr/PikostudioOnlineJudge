fb54cfe45bced8856cfa99c7708f5c2acf32b3ae
pikokr <parangee9706@gmail.com>
2020-12-23 16:54:58 +0900

    feat: format

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -2,20 +2,20 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-  uri: 'http://test.pikodev.me:1234'
+  uri: 'http://test.pikodev.me:1234',
 })
 
-const authLink = setContext((_, {headers}) => {
+const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token')
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
 })
 
 export default new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
+  link: authLink.concat(httpLink),
 })

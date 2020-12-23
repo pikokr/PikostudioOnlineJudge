f735a60b61860da9991141beb786fe476b60f409
pikokr <parangee9706@gmail.com>
2020-12-23 15:07:05 +0900

    feat: login button

--- client/src/components/Layout/LoginModal.tsx
+++ client/src/components/Layout/LoginModal.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 
 const LoginModal = ({ open, close }: { open: boolean; close: () => void }) => {
   return (
     <Modal backdrop="static" keyboard={false} onHide={close} show={open}>
       <Modal.Header closeButton>로그인</Modal.Header>
+      <Modal.Body>대충 로그인 폼</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={close} variant="danger">
+          취소
+        </Button>
+        <Button variant="success">로그인</Button>
+      </Modal.Footer>
     </Modal>
   )
 }

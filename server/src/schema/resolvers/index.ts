b8153eb33a4162154d19de55324217796b5e144b
pikokr <parangee9706@gmail.com>
2020-12-23 20:50:46 +0900

    feat: AdminProblemsMutation resolver

--- server/src/schema/resolvers/index.ts
+++ server/src/schema/resolvers/index.ts
@@ -2,9 +2,11 @@ import { IResolvers } from 'graphql-tools'
 import Query from './Query'
 import Mutation from './Mutation'
 import AdminMutation from './AdminMutation'
+import AdminProblemsMutation from './AdminProblemsMutation'
 
 export default {
   Query,
   Mutation,
-  AdminMutation
+  AdminMutation,
+  AdminProblemsMutation
 } as IResolvers

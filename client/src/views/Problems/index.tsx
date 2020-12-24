5fdc6b25f24ed0220963b933b4b42e7aefa7b5c8
pikokr <parangee9706@gmail.com>
2020-12-24 22:05:31 +0900

    feat: problem list

--- client/src/views/Problems/index.tsx
+++ client/src/views/Problems/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client'
+import _ from 'lodash'
+import Pagination from '@material-ui/lab/Pagination'
+import { Container, Spinner, Table } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Problems = ({
   match: {
     params: { page = 1 },
   },
+  history,
 }: any) => {
   if (isNaN(Number(page))) {
     return <div>페이지가 잘못되었습니다.</div>
@@ -18,7 +23,52 @@ const Problems = ({
     }
   `)
 
-  return <div>{page}</div>
+  let problems
+  let chunk
+
+  if (data) {
+    chunk = _.chunk(data.problems, 10)
+    problems = chunk[page - 1] || []
+  }
+
+  return (
+    <div>
+      {loading ? (
+        <Spinner animation="border" />
+      ) : (
+        <div>
+          {problems && chunk && problems && (
+            <Container style={{ paddingTop: 20 }}>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>제목</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {problems.map((it: any, key: number) => (
+                    <tr key={key}>
+                      <td>
+                        <Link to={`/problem/${it.id}`}>{it.id}</Link>
+                      </td>
+                      <td>
+                        <Link to={`/problem/${it.id}`}>{it.title}</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <Pagination
+                onChange={(event, page1) => history.push(`/problems/${page1}`)}
+                count={chunk.length}
+              />
+            </Container>
+          )}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Problems
